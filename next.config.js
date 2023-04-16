8b6b1c0b9332205639aae605e69905c5c5e585f7
Hyesung Oh <haesungoh414@gmail.com>
2023-04-16 17:04:02 +0900

    chore: page-extension 및 next config 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    appDir: false,
+  },
+  pageExtensions: ['page.tsx'],
+  swcMinify: true,
+  compiler: {
+    emotion: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
