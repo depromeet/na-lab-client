2e75ee8e7c371a44b113b39e8b039b913ff3abb7
Hyesung Oh <haesungoh414@gmail.com>
2023-04-22 16:08:45 +0900

    feat: use-did-update

---
+++ src/hooks/lifeCycle/useDidUpdate.ts
@@ -0,0 +1,17 @@
+import { type DependencyList, type EffectCallback, useEffect, useRef } from 'react';
+
+const useDidUpdate = (callback: EffectCallback, dependencyList: DependencyList) => {
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    }
+
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...dependencyList]);
+};
+
+export default useDidUpdate;
