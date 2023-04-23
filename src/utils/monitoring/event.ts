1eb3048284295a5de6fe0025b6ed312a281847a7
Hyesung Oh <haesungoh414@gmail.com>
2023-04-23 21:39:21 +0900

    feat: event 기록 유틸 개발

---
+++ src/utils/monitoring/event.ts
@@ -0,0 +1,18 @@
+import mixpanel from 'mixpanel-browser';
+
+import { event } from '~/libs/gtag';
+
+import { isProd } from '../common';
+
+const { WEB_VERSION } = process.env;
+
+type GAEventProps = Parameters<typeof event>[0];
+type Props = Omit<GAEventProps, 'category'>;
+
+const recordEvent = ({ action, label, value }: Props) => {
+  if (isProd(process.env.NODE_ENV)) return;
+  event({ action, category: WEB_VERSION, label, value });
+  mixpanel.track(action, { category: WEB_VERSION, label, value });
+};
+
+export default recordEvent;
