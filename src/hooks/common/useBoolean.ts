e22f36c41b928aa9b38c967b483de2559f95193a
Hyesung Oh <haesungoh414@gmail.com>
2023-04-23 19:26:03 +0900

    refactor: use-boolean return type 분리

--- src/hooks/common/useBoolean.ts
+++ src/hooks/common/useBoolean.ts
@@ -1,12 +1,14 @@
 import { useCallback, useState } from 'react';
 
+type Return = readonly [boolean, VoidFunction, VoidFunction, VoidFunction];
+
 /**
  * boolean을 쉽게 다룰 수 있는 hook 입니다
  *
  * @param initialValue - 기본 boolean 값
  * @returns [boolean state, toggle func, set true func, set false func]
  */
-const useBoolean = (initialValue: boolean): readonly [boolean, VoidFunction, VoidFunction, VoidFunction] => {
+const useBoolean = (initialValue: boolean): Return => {
   const [value, setValue] = useState(initialValue);
 
   const setTrue = useCallback(() => {
