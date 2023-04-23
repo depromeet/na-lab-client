686a57852c20f1ad04f254269dfaff60cc7feb9f
Hyesung Oh <haesungoh414@gmail.com>
2023-04-23 19:27:58 +0900

    feat: use-input tsdoc 작성 및 반환 타입 수정

--- src/hooks/common/useInput.ts
+++ src/hooks/common/useInput.ts
@@ -1,7 +1,13 @@
 import { type ChangeEventHandler, useCallback, useState } from 'react';
 
-type Return = [string, ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>, VoidFunction];
+type Return = readonly [string, ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>, VoidFunction];
 
+/**
+ * string input을 쉽게 다룰 수 있는 hook 입니다
+ *
+ * @param initialValue - 기본 string 값
+ * @returns [string state, on change handler, state reset func]
+ */
 const useInput = (initialValue = ''): Return => {
   const [state, setState] = useState(initialValue);
 
