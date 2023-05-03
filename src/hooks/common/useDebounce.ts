b7c790becfc54c0be5af9791b887ae6eb71c95ba
sjoleee <cent7425@gmail.com>
2023-05-04 03:47:13 +0900

    chore: jsdoc

--- src/hooks/common/useDebounce.ts
+++ src/hooks/common/useDebounce.ts
@@ -1,6 +1,13 @@
 import { useRef } from 'react';
 
-const useDebounce = (cb: () => void, ms: number) => {
+/**
+ * 함수에 debounce를 적용하는 hook입니다. ms 이내의 실행을 병합하여 한 번 실행합니다.
+ *
+ * @param cb debounce 적용을 원하는 함수
+ * @param ms debounce timeout
+ * @returns debouncedFn
+ */
+const useDebounce = (cb: () => void, ms: number): (() => void) => {
   const timer = useRef<ReturnType<typeof setTimeout>>();
 
   const dispatchDebounce = () => {
