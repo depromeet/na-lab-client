432d76b8e9d4b25fa3946f4540525c15434a7bd6
Hyesung Oh <haesungoh414@gmail.com>
2023-05-03 14:30:39 +0900

    feat: bottom-sheet

---
+++ src/components/portal/BottomSheet.tsx
@@ -0,0 +1,75 @@
+import { type ComponentProps, type MouseEventHandler } from 'react';
+import { css } from '@emotion/react';
+import { m, type Variants } from 'framer-motion';
+
+import { defaultEasing, defaultFadeInVariants } from '~/constants/motions';
+
+import AnimatePortal from './AnimatePortal';
+import { scrimCss } from './scrim.style';
+
+interface Props extends ComponentProps<typeof AnimatePortal> {
+  onClickScrim?: VoidFunction;
+}
+
+const BottomSheet = ({ onClickScrim, isShowing, children, mode }: Props) => {
+  const onClickScrimDefault: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (e.target !== e.currentTarget) return;
+    if (onClickScrim) onClickScrim();
+  };
+
+  return (
+    <AnimatePortal isShowing={isShowing} mode={mode}>
+      <m.div
+        onClick={onClickScrimDefault}
+        css={scrimCss}
+        variants={defaultFadeInVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      >
+        <m.div css={contentCss} variants={bottomSheetVariants}>
+          {children}
+        </m.div>
+      </m.div>
+    </AnimatePortal>
+  );
+};
+
+export default BottomSheet;
+
+const contentCss = css`
+  position: absolute;
+  z-index: 1000;
+  top: 100%;
+  left: 0;
+
+  width: 100%;
+
+  /* TODO: 디자인에 따라 변경 필요 */
+  min-height: 400px;
+  max-height: 95%;
+  padding-top: 16px;
+  padding-right: 20px;
+  padding-left: 20px;
+
+  /* TODO: 디자인에 따라 변경 필요 */
+  background-color: white;
+`;
+
+const bottomSheetVariants: Variants = {
+  initial: {
+    y: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'transform',
+  },
+  animate: {
+    y: '-100%',
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'transform',
+  },
+  exit: {
+    y: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'transform',
+  },
+};
