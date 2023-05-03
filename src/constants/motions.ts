f6e4ff55fd36e461fe20ae42e58827ec3ab285e3
Hyesung Oh <haesungoh414@gmail.com>
2023-05-03 13:17:15 +0900

    chore: 모션 상수 선언

---
+++ src/constants/motions.ts
@@ -0,0 +1,29 @@
+import { type Variants } from 'framer-motion';
+
+export const defaultEasing = [0.6, -0.05, 0.01, 0.99];
+
+export const staggerOne: Variants = {
+  animate: { transition: { staggerChildren: 0.1 } },
+};
+
+export const staggerHalf: Variants = {
+  animate: { transition: { staggerChildren: 0.05 } },
+};
+
+export const defaultFadeInVariants: Variants = {
+  initial: {
+    opacity: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity',
+  },
+  animate: {
+    opacity: 1,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity',
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity',
+  },
+};
