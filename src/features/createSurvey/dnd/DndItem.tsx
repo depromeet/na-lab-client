97c4f5e35637208f68666e64cf8130006a324b16
sumi-0011 <selina2000@naver.com>
2023-05-09 00:49:43 +0900

    feat: icon을 이용해서 drag하도록 변경

--- src/features/createSurvey/dnd/DndItem.tsx
+++ src/features/createSurvey/dnd/DndItem.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable unicorn/filename-case */
 import React from 'react';
-import { Reorder } from 'framer-motion';
+import { css } from '@emotion/react';
+import { Reorder, useDragControls } from 'framer-motion';
+
+import ReorderIcon from './ReorderIcon';
 
 interface Props {
   item: {
@@ -10,11 +13,32 @@ interface Props {
   };
 }
 const DndItem = ({ item }: Props) => {
+  const dragControls = useDragControls();
+
   return (
-    <Reorder.Item value={item} as="div">
-      <div className="reorder-handle">
-        <div>{item.content}</div>
-        <div>{item.title}</div>
+    <Reorder.Item value={item} as="div" dragListener={false} dragControls={dragControls}>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+
+          width: fit-content;
+          padding: 10px;
+
+          border: 1px solid #ddd;
+        `}
+      >
+        <div
+          className="reorder-handle"
+          css={css`
+            user-select: none;
+            width: 300px;
+          `}
+        >
+          <div>{item.content}</div>
+          <div>{item.title}</div>
+        </div>
+        <ReorderIcon dragControls={dragControls} />
       </div>
     </Reorder.Item>
   );
