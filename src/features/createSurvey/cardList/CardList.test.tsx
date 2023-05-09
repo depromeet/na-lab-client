8d229694b33d13dd8054611bff84321db6957d54
sumi-0011 <selina2000@naver.com>
2023-05-10 00:22:28 +0900

    test: 필요없는 mock 함수 제거

--- src/features/createSurvey/cardList/CardList.test.tsx
+++ src/features/createSurvey/cardList/CardList.test.tsx
@@ -1,18 +1,7 @@
-import { type PropsWithChildren } from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
 import { afterEach, describe, expect, test, vi } from 'vitest';
 
 import CardList from '~/features/createSurvey/cardList/CardList';
-
-vi.mock('framer-motion', async () => {
-  const actual = await vi.importActual('framer-motion');
-  return {
-    ...(actual as object),
-    AnimatePresence: ({ children }: PropsWithChildren) => (
-      <div className="mocked-framer-motion-AnimatePresence">{children}</div>
-    ),
-  };
-});
 
 describe('/features/createSurvey/cardList', () => {
   test('정의되어 있어야 한다', () => {
