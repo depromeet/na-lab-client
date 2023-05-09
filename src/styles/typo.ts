e8f8b8e7c1a7624351621f3f209e8175200b86cc
Hyesung Oh <haesungoh414@gmail.com>
2023-05-10 00:19:20 +0900

    feat: typo 선언

---
+++ src/styles/typo.ts
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react';
+
+export const HEAD_1 = css`
+  font-weight: Bold;
+  font-size: 1.5rem;
+  line-height: 138%;
+  letter-spacing: -0.3px;
+`;
+
+export const HEAD_2_BOLD = css`
+  font-weight: SemiBold;
+  font-size: 1.125rem;
+  line-height: 140%;
+  letter-spacing: -0.3px;
+`;
+
+export const HEAD_2_REGULAR = css`
+  font-weight: Regular;
+  font-size: 1.125rem;
+  line-height: 150%;
+  letter-spacing: -0.3px;
+`;
+
+export const BODY_1 = css`
+  font-weight: Regular;
+  font-size: 1rem;
+  line-height: 150%;
+  letter-spacing: -0.3px;
+`;
+
+export const BODY_2_BOLD = css`
+  font-weight: SemiBold;
+  font-size: 0.875rem;
+  line-height: 140%;
+  letter-spacing: -0.3px;
+`;
+
+export const BODY_2_REGULAR = css`
+  font-weight: Regular;
+  font-size: 0.875rem;
+  line-height: 140%;
+  letter-spacing: -0.3px;
+`;
