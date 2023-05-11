2dbbde74dd1759840add3002a2db7ede6e62260f
oyeon-kwon <rnjsdhdus93@gmail.com>
2023-05-11 20:54:00 +0900

    feat: next-auth kakao login

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+export default NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: '',
+      clientSecret: '',
+    }),
+  ],
+});
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,12 @@
+import { signIn } from 'next-auth/react';
+
 export default function Home() {
-  return <div>home</div>;
+  // const { data: session } = useSession();
+  return (
+    <div>
+      <button type="button" onClick={() => signIn('kakao')}>
+        카카오로 로그인 하기
+      </button>
+    </div>
+  );
 }
