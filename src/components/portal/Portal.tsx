4ebd6b502d0cb9741abbbbe61aab75a3237fcbb5
sjoleee <cent7425@gmail.com>
2023-05-13 03:16:14 +0900

    fix: portal 컴포넌트 수정

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -1,13 +1,21 @@
-import { type PropsWithChildren } from 'react';
+import { type PropsWithChildren, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 /**
  * @description react.createPortal을 이용해 document.body에 children을 렌더링합니다
  */
 const Portal = ({ children }: PropsWithChildren) => {
-  const container = typeof window !== 'undefined' && document.body;
+  const [container, setContainer] = useState<Element | null>(null);
 
-  return container ? createPortal(children, container) : null;
+  useEffect(() => {
+    if (document) {
+      setContainer(document.body);
+    }
+  }, []);
+
+  if (!container) return null;
+
+  return createPortal(children, container);
 };
 
 export default Portal;
