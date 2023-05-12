648aeff9508f5bff49132d62917c3887adbdec9e
oyeon-kwon <rnjsdhdus93@gmail.com>
2023-05-12 17:01:27 +0900

    feat: next auth kakao login 구현 및 테스트 코드 추가

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ const nextConfig = {
     appDir: false,
     forceSwcTransforms: true,
   },
-  pageExtensions: ['page.tsx'],
   swcMinify: true,
   compiler: {
     emotion: true,
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import KakaoProvider from 'next-auth/providers/kakao';
 export default NextAuth({
   providers: [
     KakaoProvider({
-      clientId: '',
-      clientSecret: '',
+      clientId: process.env.KAKAO_CLIENT_ID || '',
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || '',
     }),
   ],
 });
