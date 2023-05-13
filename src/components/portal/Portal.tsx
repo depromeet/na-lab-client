8d153b1f9aaea442017080fb04725f484d3b1fed
sjoleee <cent7425@gmail.com>
2023-05-13 13:17:16 +0900

    feat: useDidMount 사용

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -1,5 +1,7 @@
-import { type PropsWithChildren, useEffect, useState } from 'react';
+import { type PropsWithChildren, useState } from 'react';
 import { createPortal } from 'react-dom';
+
+import useDidMount from '~/hooks/lifeCycle/useDidMount';
 
 /**
  * @description react.createPortal을 이용해 document.body에 children을 렌더링합니다
@@ -7,11 +9,11 @@ import { createPortal } from 'react-dom';
 const Portal = ({ children }: PropsWithChildren) => {
   const [container, setContainer] = useState<Element | null>(null);
 
-  useEffect(() => {
+  useDidMount(() => {
     if (document) {
       setContainer(document.body);
     }
-  }, []);
+  });
 
   if (!container) return null;
 
