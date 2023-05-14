0c4ed9475f2d05a031b67b5ab036588ee29024a5
Sangjo LEE <cent7425@gmail.com>
2023-05-14 22:00:41 +0900

    Portal 컴포넌트 hydration 오류 수정 (#65)
    * fix: portal 컴포넌트 수정
    
    * feat: useDidMount 사용

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -1,13 +1,23 @@
-import { type PropsWithChildren } from 'react';
+import { type PropsWithChildren, useState } from 'react';
 import { createPortal } from 'react-dom';
+
+import useDidMount from '~/hooks/lifeCycle/useDidMount';
 
 /**
  * @description react.createPortal을 이용해 document.body에 children을 렌더링합니다
  */
 const Portal = ({ children }: PropsWithChildren) => {
-  const container = typeof window !== 'undefined' && document.body;
+  const [container, setContainer] = useState<Element | null>(null);
 
-  return container ? createPortal(children, container) : null;
+  useDidMount(() => {
+    if (document) {
+      setContainer(document.body);
+    }
+  });
+
+  if (!container) return null;
+
+  return createPortal(children, container);
 };
 
 export default Portal;
