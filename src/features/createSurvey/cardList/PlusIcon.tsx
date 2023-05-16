56876c71039c30b398abd76ed58eaa85c8a82b30
sumi.byun <selina2000@naver.com>
2023-05-16 22:46:38 +0900

    Drag and Drop Component 만들기 (#49)
    * feat: dnd 컴포넌트 생성
    
    * feat: icon을 이용해서 drag하도록 변경
    
    * feat: dnd 스토리북 추가
    
    * feat: card component로 변경, style 적용
    
    * feat: card type 추가, 질문 추가 버튼 활성화 (임시)
    
    * test: cardList 드래그 앤 드롭 테스트 동작하는지 테스트 코드 작성
    
    * test: 추가 버튼 클릭시 card item 추가 테스트
    
    * test: 필요없는 mock 함수 제거
    
    * Update src/features/createSurvey/cardList/Card.tsx
    
    fix: 상수 객체 동결
    
    Co-authored-by: hyesung oh <haesungoh414@gmail.com>
    
    * feat: 혜성님 리뷰 적용
    
    * refactor: 컴포넌트  CardList, CardListWithDnd 분리
    
    * test: cardWithDnd, CreateSurvey 테스트 분리
    
    * test: card view 테스트 코드 작성
    
    * fix: card list 스토리북 수정, with dnd 추가
    
    ---------
    
    Co-authored-by: hyesung oh <haesungoh414@gmail.com>

---
+++ src/features/createSurvey/cardList/PlusIcon.tsx
@@ -0,0 +1,10 @@
+const PlusIcon = () => {
+  return (
+    <svg width="27" height="26" viewBox="0 0 27 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M26.2285 12.7266L0.774427 12.7266" stroke="#37C3FF" strokeWidth="2" />
+      <path d="M13.498 25.4551V0.000989047" stroke="#37C3FF" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export default PlusIcon;
