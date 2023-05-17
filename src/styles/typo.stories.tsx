27963bacb4ad47b4a0c48232d699b88cf1419ae8
sumi-0011 <selina2000@naver.com>
2023-05-17 22:19:56 +0900

    feat: 스토리북 추가 - HEAD_3_SEMIBOLD

--- src/styles/typo.stories.tsx
+++ src/styles/typo.stories.tsx
@@ -2,7 +2,7 @@ import { type Meta } from '@storybook/react';
 
 import { pretendard } from '~/assets/fonts/pretendard';
 
-import { BODY_1, BODY_2_BOLD, BODY_2_REGULAR, HEAD_1, HEAD_2_BOLD, HEAD_2_REGULAR } from './typo';
+import { BODY_1, BODY_2_BOLD, BODY_2_REGULAR, HEAD_1, HEAD_2_BOLD, HEAD_2_REGULAR, HEAD_3_SEMIBOLD } from './typo';
 
 const meta: Meta<typeof Default> = {
   title: 'Typographies',
@@ -22,6 +22,9 @@ export function Default() {
 
       <p>HEAD 2 REGULAR</p>
       <p css={HEAD_2_REGULAR}>동료의 익명 피드백으로 완성시키는 나의 커리어 브랜딩 서비스</p>
+
+      <p>HEAD_3_SEMIBOLD</p>
+      <p css={HEAD_3_SEMIBOLD}>동료의 익명 피드백으로 완성시키는 나의 커리어 브랜딩 서비스</p>
 
       <p>BODY 1</p>
       <p css={BODY_1}>동료의 익명 피드백으로 완성시키는 나의 커리어 브랜딩 서비스</p>
