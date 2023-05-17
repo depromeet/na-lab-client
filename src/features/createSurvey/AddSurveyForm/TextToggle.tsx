91da81021f845c7eed6165ac04a8811f5c11fa39
sumi-0011 <selina2000@naver.com>
2023-05-17 22:21:11 +0900

    feat: text toggle component 생성

---
+++ src/features/createSurvey/AddSurveyForm/TextToggle.tsx
@@ -0,0 +1,79 @@
+import { css, type Theme } from '@emotion/react';
+import { motion } from 'framer-motion';
+
+import { HEAD_3_SEMIBOLD } from '~/styles/typo';
+
+interface Props {
+  list: {
+    type: string;
+    label: string;
+  }[];
+  selectItem: string;
+  onItemClick: (type: string) => void;
+}
+
+function TextToggle({ selectItem, list, onItemClick }: Props) {
+  return (
+    <div>
+      <motion.div animate css={containerCss}>
+        {list.map(({ label, type }) => {
+          const isSelected = type === selectItem;
+
+          return (
+            <button type="button" onClick={() => onItemClick(type)} css={itemCss} key={label}>
+              {isSelected ? <motion.div animate css={itemBoxCss} layoutId="underline" /> : null}
+              <span
+                css={[HEAD_3_SEMIBOLD, (theme) => ({ color: isSelected ? theme.colors.white : theme.colors.gray_400 })]}
+              >
+                {label}
+              </span>
+            </button>
+          );
+        })}
+      </motion.div>
+    </div>
+  );
+}
+
+const itemCss = (theme: Theme) => css`
+  position: relative;
+
+  width: 105px;
+  height: 32px;
+
+  background-color: ${theme.colors.gray_50};
+  border: none;
+  border-radius: 24px;
+
+  span {
+    position: relative;
+    z-index: 2;
+  }
+`;
+
+const itemBoxCss = (theme: Theme) => css`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
+
+  width: 105px;
+  height: 32px;
+
+  background-color: ${theme.colors.primary_200};
+  border-radius: 24px;
+`;
+
+const containerCss = (theme: Theme) => css`
+  display: flex;
+  gap: 4px;
+
+  width: fit-content;
+  height: 42px;
+  padding: 5px;
+
+  background-color: ${theme.colors.gray_50};
+  border-radius: 34px;
+`;
+
+export default TextToggle;
