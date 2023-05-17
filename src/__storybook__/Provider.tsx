947e9bfcb42241f88cb2b9f0e05e5dab08e708c6
Hyesung Oh <haesungoh414@gmail.com>
2023-05-18 02:36:38 +0900

    test: 스토리북 provider에 기본 레이아웃 적용

--- src/__storybook__/Provider.tsx
+++ src/__storybook__/Provider.tsx
@@ -1,18 +1,26 @@
 import { type PropsWithChildren } from 'react';
-import { ThemeProvider } from '@emotion/react';
+import { css, type Theme, ThemeProvider } from '@emotion/react';
 
 import { pretendard } from '~/assets/fonts/pretendard';
-import theme from '~/styles/theme';
+import defaultTheme from '~/styles/theme';
 
 /**
  * @description 스토리북에서 사용하는 Provider 컴포넌트입니다.
  */
 const Provider = ({ children }: PropsWithChildren) => {
   return (
-    <ThemeProvider theme={theme}>
-      <div className={pretendard.className}>{children}</div>
+    <ThemeProvider theme={defaultTheme}>
+      <div css={defaultLayoutCss} className={pretendard.className}>
+        {children}
+      </div>
     </ThemeProvider>
   );
 };
 
 export default Provider;
+
+const defaultLayoutCss = (theme: Theme) => css`
+  width: 100%;
+  max-width: ${theme.size.maxWidth};
+  margin: 0 auto;
+`;
