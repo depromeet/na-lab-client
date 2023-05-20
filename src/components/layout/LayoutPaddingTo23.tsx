756b070e221ca9a29ff5c35d6c7b842c4d74f6bc
Hyesung Oh <haesungoh414@gmail.com>
2023-05-20 22:31:38 +0900

    feat: layout padding to 23 컴포넌트

---
+++ src/components/layout/LayoutPaddingTo23.tsx
@@ -0,0 +1,26 @@
+import { type PropsWithChildren } from 'react';
+import { css } from '@emotion/react';
+
+/**
+ * @description `_app`에서 적용된 `padding: 0 16px`에서 추가적으로 7px을 더해 23px로 만드는 용의 wrapper 입니다
+ * @example
+ * ```tsx
+ * // index.page.tsx
+ * import LayoutPaddingTo23 from '~/components/layout/LayoutPaddingTo23';
+ *
+ * export default function Index() {
+ *  return <div>foo</div>;
+ * }
+ *
+ * Index.getLayout = (page) => <LayoutPaddingTo23>{page}</LayoutPaddingTo23>;
+ * ```
+ */
+const LayoutPaddingTo23 = ({ children }: PropsWithChildren) => {
+  return <div css={layoutCss}>{children}</div>;
+};
+
+export default LayoutPaddingTo23;
+
+const layoutCss = css`
+  padding: 0 7px;
+`;
