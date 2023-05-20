2a998a26985d017935d9a748b7101d170b4a739c
Hyesung Oh <haesungoh414@gmail.com>
2023-05-20 18:55:18 +0900

    test: with animate presence

--- src/components/text/StaggerParagraph.stories.tsx
+++ src/components/text/StaggerParagraph.stories.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { css } from '@emotion/react';
 import { type Meta } from '@storybook/react';
+import { AnimatePresence } from 'framer-motion';
 
 import { defaultFadeInVariants, staggerHalf } from '~/constants/motions';
 
@@ -41,5 +43,48 @@ export function Override() {
       <p>두 번째 문장</p>
       <p>세 번째 문장</p>
     </StaggerParagraph>
+  );
+}
+
+export function WithAnimatePresence() {
+  const [step, setStep] = useState(0);
+
+  const prev = () => {
+    if (step === 0) return;
+    setStep((p) => p - 1);
+  };
+
+  const next = () => {
+    if (step === 1) return;
+    setStep((p) => p + 1);
+  };
+
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {step === 0 && (
+          <StaggerParagraph key={0}>
+            <p>첫 번째 문장</p>
+            <p>두 번째 문장</p>
+            <p>세 번째 문장</p>
+          </StaggerParagraph>
+        )}
+
+        {step === 1 && (
+          <StaggerParagraph key={1}>
+            <p>네 번째 문장</p>
+            <p>다섯 번째 문장</p>
+            <p>여섯 번째 문장</p>
+          </StaggerParagraph>
+        )}
+      </AnimatePresence>
+
+      <button type="button" onClick={prev}>
+        prev
+      </button>
+      <button type="button" onClick={next}>
+        next
+      </button>
+    </>
   );
 }
