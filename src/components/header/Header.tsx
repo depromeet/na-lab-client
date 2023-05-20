fdbc83ea844034f579e8703beb09c9e2e223bbca
sumi.byun <selina2000@naver.com>
2023-05-20 14:39:44 +0900

    Header 컴포넌트 생성 (#77)
    * feat: header 컴포넌트 생성
    
    * feat: 혜성님 리뷰 반영

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,66 @@
+import { useRouter } from 'next/router';
+import { css, type Theme } from '@emotion/react';
+
+import ArrowIcon from '~/components/icons/ArrowIcon';
+import { HEAD_2_BOLD } from '~/styles/typo';
+
+interface Props {
+  title?: string;
+  rightButton?: React.ReactNode;
+  onBackClick?: () => void;
+}
+
+const Header = ({ title, rightButton, onBackClick }: Props) => {
+  const router = useRouter();
+
+  const onBack = () => {
+    onBackClick ? onBackClick() : router.back();
+  };
+
+  return (
+    <header css={[headerCss, fixedTopCss]}>
+      <button type="button" onClick={onBack} css={iconButtonCss}>
+        <ArrowIcon />
+      </button>
+      {title && <h1 css={[HEAD_2_BOLD, titleCss]}>{title}</h1>}
+      {rightButton}
+    </header>
+  );
+};
+
+export default Header;
+
+const iconButtonCss = css`
+  all: unset;
+  cursor: pointer;
+`;
+
+const titleCss = css`
+  position: absolute;
+  right: 0;
+  left: 0;
+
+  width: fit-content;
+  margin: auto;
+`;
+
+const fixedTopCss = (theme: Theme) => css`
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  left: 0;
+
+  width: 100%;
+  max-width: ${theme.size.maxWidth};
+  margin: 0 auto;
+`;
+
+const headerCss = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 48px;
+  padding: 16px;
+`;
