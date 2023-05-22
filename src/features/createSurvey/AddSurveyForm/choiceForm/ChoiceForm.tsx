1cc638f2e1f5570d8b33c48bbf20b9e092cf09ea
sumi-0011 <selina2000@naver.com>
2023-05-23 02:12:26 +0900

    feat: 복수 선택 애니매이션 추가

--- src/features/createSurvey/AddSurveyForm/choiceForm/ChoiceForm.tsx
+++ src/features/createSurvey/AddSurveyForm/choiceForm/ChoiceForm.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { css } from '@emotion/react';
+import { m } from 'framer-motion';
 
+import { defaultFadeInDownVariants } from '~/constants/motions';
 import Check from '~/features/createSurvey/addSurveyForm/choiceForm/Check';
 import MaximumSelect from '~/features/createSurvey/addSurveyForm/choiceForm/MaximumSelect';
 import SelectTextFieldList from '~/features/createSurvey/addSurveyForm/selectionTextfieldList/SelectionTextfieldList';
@@ -9,6 +11,7 @@ import useDidUpdate from '~/hooks/lifeCycle/useDidUpdate';
 
 const ChoiceForm = () => {
   const [isChecked, toggleCheck] = useBoolean(false);
+
   const [maxSelect, setMaxSelect] = useState(1);
   const [inputs, setInputs] = useState(['', '']);
 
@@ -21,7 +24,11 @@ const ChoiceForm = () => {
   return (
     <div css={containerCss}>
       <Check isChecked={isChecked} toggleCheck={toggleCheck} label="복수선택 가능" />
-      <MaximumSelect value={maxSelect} setValue={setMaxSelect} />
+      {isChecked && (
+        <m.div variants={defaultFadeInDownVariants} initial="initial" animate="animate" exit="exit">
+          <MaximumSelect value={maxSelect} setValue={setMaxSelect} />
+        </m.div>
+      )}
       <SelectTextFieldList inputs={inputs} basicCount={maxSelect} setInputs={setInputs} />
     </div>
   );
