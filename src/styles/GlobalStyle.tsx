b2efc08d0d781f6ddd71bda9d2e53ba31076873f
Hyesung Oh <haesungoh414@gmail.com>
2023-05-23 01:02:38 +0900

    style: all unset으로 대응

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,152 +1,9 @@
 import { css, Global } from '@emotion/react';
 
-const resetCss = css`
-  html,
-  body,
-  div,
-  span,
-  applet,
-  object,
-  iframe,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
-  blockquote,
-  pre,
-  a,
-  abbr,
-  acronym,
-  address,
-  big,
-  cite,
-  code,
-  del,
-  dfn,
-  em,
-  img,
-  ins,
-  kbd,
-  q,
-  s,
-  samp,
-  small,
-  strike,
-  strong,
-  sub,
-  sup,
-  tt,
-  var,
-  b,
-  u,
-  i,
-  center,
-  dl,
-  dt,
-  dd,
-  ol,
-  ul,
-  li,
-  fieldset,
-  form,
-  label,
-  legend,
-  table,
-  caption,
-  tbody,
-  tfoot,
-  thead,
-  tr,
-  th,
-  td,
-  article,
-  aside,
-  canvas,
-  details,
-  embed,
-  figure,
-  figcaption,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  output,
-  ruby,
-  section,
-  summary,
-  time,
-  mark,
-  audio,
-  video {
-    margin: 0;
-    padding: 0;
-
-    font-size: 100%;
-    vertical-align: baseline;
-
-    border: 0;
-  }
-
-  article,
-  aside,
-  details,
-  figcaption,
-  figure,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  section {
-    display: block;
-  }
-
-  body {
-    line-height: 1;
-  }
-
-  ol,
-  ul {
-    list-style: none;
-  }
-
-  blockquote,
-  q {
-    quotes: none;
-  }
-
-  blockquote::before,
-  blockquote::after,
-  q::before,
-  q::after {
-    content: '';
-    content: none;
-  }
-
-  button {
-    cursor: pointer;
-    background-color: unset;
-    border: none;
-  }
-
-  a {
-    cursor: pointer;
-  }
-
-  table {
-    border-spacing: 0;
-    border-collapse: collapse;
-  }
-`;
-
 const globalStyles = css`
-  ${resetCss};
-
   * {
+    all: unset;
+
     box-sizing: border-box !important;
     margin: 0;
     padding: 0;
@@ -157,8 +14,11 @@ const globalStyles = css`
   }
 
   a {
-    color: inherit;
-    text-decoration: none;
+    cursor: pointer;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
 
