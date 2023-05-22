55ce6b1b08190018ee8b8011e3ae369487beb2c3
sjoleee <cent7425@gmail.com>
2023-05-23 01:03:37 +0900

    feat: feedback 컴포넌트 작성

---
+++ src/constants/position.ts
@@ -0,0 +1,6 @@
+export const POSITION = {
+  'product-manager': '기획자',
+  developer: '개발자',
+  designer: '디자이너',
+  other: '지인',
+};
--- src/features/feedback/UnreadBadgeIcon.tsx
+++ src/features/feedback/UnreadBadgeIcon.tsx
@@ -3,23 +3,33 @@ import { css } from '@emotion/react';
 import Svg from '~/components/svg/Svg';
 
 interface Props {
+  size?: 'small' | 'large';
   floatingTop?: string;
   floatingBottom?: string;
   floatingLeft?: string;
   floatingRight?: string;
 }
 
-const UnreadBadgeIcon = ({ floatingTop, floatingBottom, floatingLeft, floatingRight }: Props) => {
+const UnreadBadgeIcon = ({ size = 'large', floatingTop, floatingBottom, floatingLeft, floatingRight }: Props) => {
   return (
-    <Svg width={10} height={10} css={floatingCss({ floatingTop, floatingBottom, floatingLeft, floatingRight })}>
-      <circle cx="5" cy="5" r="5" fill="#F85B81" />
+    <Svg
+      width={size === 'small' ? 8 : 10}
+      height={size === 'small' ? 8 : 10}
+      css={floatingCss({ floatingTop, floatingBottom, floatingLeft, floatingRight })}
+    >
+      <circle
+        cx={size === 'small' ? '4' : '5'}
+        cy={size === 'small' ? '4' : '5'}
+        r={size === 'small' ? '4' : '5'}
+        fill="#F85B81"
+      />
     </Svg>
   );
 };
 
 export default UnreadBadgeIcon;
 
-const floatingCss = ({ floatingTop, floatingBottom, floatingLeft, floatingRight }: Props) => css`
+const floatingCss = ({ floatingTop, floatingBottom, floatingLeft, floatingRight }: Omit<Props, 'size'>) => css`
   position: absolute;
   inset: ${floatingTop ?? 'auto'} ${floatingRight ?? 'auto'} ${floatingBottom ?? 'auto'} ${floatingLeft ?? 'auto'};
 `;
