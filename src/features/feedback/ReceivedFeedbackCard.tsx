4e931025eb24f25f05b8b75198ad678e1c514ca6
oyeon-kwon <rnjsdhdus93@gmail.com>
2023-05-26 02:11:21 +0900

    fix: 3D 이미지 변경

--- src/features/feedback/ReceivedFeedbackCard.tsx
+++ src/features/feedback/ReceivedFeedbackCard.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { css } from '@emotion/react';
 
 import colors from '~/styles/color';
 import { HEAD_2_BOLD } from '~/styles/typo';
 
-import feedbackImage from '../../../public/images/feedbackImage.png';
+import blueProgrammer from '../../../public/images/blue-programmer.png';
+import greenPm from '../../../public/images/green-pm.png';
+import pinkDesigner from '../../../public/images/pink-designer.png';
+import Purple from '../../../public/images/purple.png';
 import CollaborationBadge from './CollaborationBadge';
 import UnreadBadgeIcon from './UnreadBadgeIcon';
 
-const ReceivedFeedbackCard = ({ isRead = false, feedbackUser = '개발자 A', isCollaborate = true }) => {
+interface Props {
+  isRead?: boolean;
+  feedbackUser?: string;
+  isCollaborate?: boolean;
+}
+
+const ReceivedFeedbackCard = ({ isRead = false, feedbackUser = '개발자', isCollaborate = true }: Props) => {
+  const [roleImage, setRoleImage] = useState(blueProgrammer);
+
+  const changeImageCardByRole = (roleInput: string) => {
+    if (roleInput === '개발자') {
+      setRoleImage(blueProgrammer);
+    } else if (roleInput === '디자이너') {
+      setRoleImage(pinkDesigner);
+    } else if (roleInput === '기획자') {
+      setRoleImage(greenPm);
+    } else {
+      setRoleImage(Purple);
+    }
+  };
+
+  useEffect(() => {
+    changeImageCardByRole(feedbackUser);
+  }, []);
+
   return (
     <section css={containerCss}>
       {isRead ? null : <UnreadBadgeIcon floatingTop="8px" floatingRight="8px" />}
 
       <figure css={BodyCss}>
-        {/* 
-        // TODO: 이미지는 추후 3D로 변경 예정
-        */}
-        <Image css={ImageCss} src={feedbackImage} alt="피드백 이미지" />
+        <Image css={ImageCss} src={roleImage} alt="포지션별 이미지" />
         <div css={[HEAD_2_BOLD, DescCss]}>
           {feedbackUser}의<br />
           피드백
@@ -53,16 +77,16 @@ const BodyCss = css`
 `;
 
 const ImageCss = css`
-  width: 60px;
-  height: 50px;
-  margin: 26px auto auto;
+  width: 65px;
+  height: 60px;
+  margin: 20px auto auto;
   border-radius: 5px;
 `;
 
 const DescCss = css`
   display: flex;
 
-  margin: 21px auto 0;
+  margin: 16px auto 0;
 
   font-size: 18px;
   font-weight: 600;
@@ -76,5 +100,5 @@ const FooterCss = css`
   justify-content: center;
 
   height: 35px;
-  margin-top: 11px;
+  margin-top: 8px;
 `;
