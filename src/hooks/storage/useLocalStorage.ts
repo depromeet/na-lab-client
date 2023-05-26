957387ea894b7212bef7674c6d2543c576f02f76
sumi-0011 <selina2000@naver.com>
2023-05-27 01:21:51 +0900

    refactor: 혜성님 리뷰 반영

--- src/hooks/storage/useLocalStorage.ts
+++ src/hooks/storage/useLocalStorage.ts
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
+
+import useDidMount from '~/hooks/lifeCycle/useDidMount';
 
 /**
  * @description 페이지 새로 고침을 통해 상태가 유지되도록 로컬 저장소에 동기화합니다.
@@ -8,8 +10,6 @@ import { useEffect, useState } from 'react';
  * @returns [storedValue, setValue] - 로컬 저장소에 저장된 값, 저장 함수
  */
 function useLocalStorage<T>(key: string, initialValue: T) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
     if (typeof window === 'undefined') {
       return initialValue;
@@ -37,16 +37,17 @@ function useLocalStorage<T>(key: string, initialValue: T) {
     }
   };
 
-  const [hasMounted, setHasMounted] = useState(false);
+  const isMounted = useRef(false);
 
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
+  useDidMount(() => {
+    isMounted.current = true;
+  });
 
-  if (hasMounted) {
+  if (isMounted.current) {
     return [storedValue, setValue] as const;
   }
 
+  // mount 되기 이전에는 초기 값 반환
   return [initialValue, setValue] as const;
 }
 
