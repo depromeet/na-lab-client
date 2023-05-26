c6bf543c51f81c36aa8f1f4ecc4282df3408728e
sjoleee <cent7425@gmail.com>
2023-05-26 18:12:21 +0900

    chore: jsdoc

--- src/components/toast/useToast.ts
+++ src/components/toast/useToast.ts
@@ -1,5 +1,14 @@
 import { useRef, useState } from 'react';
 
+/**
+ * toast 사용을 위한 hook입니다.
+ *
+ * @param duration toast의 지속시간
+ * @return isShowing toast의 상태
+ * @return showToast 실행하면 toast가 켜짐
+ * @return closeToast 실행하면 toast가 닫힘
+ * @return setIsShowing isShowing을 직접 업데이트할때 사용하는 setter
+ */
 const useToast = (duration = 2000) => {
   const [isShowing, setIsShowing] = useState(false);
   const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
@@ -26,9 +35,9 @@ const useToast = (duration = 2000) => {
 
   return {
     isShowing,
-    setIsShowing,
     showToast,
     closeToast,
+    setIsShowing,
   };
 };
 
