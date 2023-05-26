8080b538b5022e3c98dbaabe5a9f718bdeef5180
Hyesung Oh <haesungoh414@gmail.com>
2023-05-27 01:33:05 +0900

    feat: 강점 스탭 및 객관식 질문 폼

--- src/pages/review/[token].page.tsx
+++ src/pages/review/[token].page.tsx
@@ -4,6 +4,7 @@ import { css } from '@emotion/react';
 import { AnimatePresence } from 'framer-motion';
 
 import Intro from '~/features/review/steps/Intro';
+import ShortQuestion from '~/features/review/steps/ShortQuestion';
 import useInjectedElementStep from '~/hooks/step/useInjectedElementStep';
 
 const Cowork = dynamic(() => import('~/features/review/steps/Cowork'), { ssr: false });
@@ -14,11 +15,22 @@ const ReviewPage = () => {
   // const { token } = router.query;
 
   const { isCoworked, setIsCoworked } = useIsCowork();
+  const { setStrength } = useStrength();
 
   const { currentElement } = useInjectedElementStep({
     elements: [
       <Intro key="intro" />,
       <Cowork key="cowork" isCoworked={isCoworked} setIsCoworked={setIsCoworked} />,
+      <ShortQuestion
+        key="strength"
+        headerTitle="협업을 하면서 느꼈던 예진님만의 장점이 있나요?"
+        setReplies={setStrength}
+        startMessage={[
+          { timing: 1000, text: '협업을 한 적이 없다면 일상에서 드러나는 성격이나 행동에 대한 장점을 적어주세요.' },
+          { timing: 2000, text: '답변을 적어 저에게 메세지를 보내주시면, 익명으로 전달할게요!' },
+        ]}
+        afterUserMessage={[{ timing: 1000, text: '못한 말이 있다면 더 보낼 수 있어요.' }]}
+      />,
       <div key="div">2</div>,
       <div key="div2">3</div>,
     ],
@@ -43,4 +55,10 @@ const useIsCowork = () => {
   const [isCoworked, setIsCoworked] = useState<null | boolean>(null);
 
   return { isCoworked, setIsCoworked };
+};
+
+const useStrength = () => {
+  const [strength, setStrength] = useState<string[]>([]);
+
+  return { strength, setStrength };
 };
