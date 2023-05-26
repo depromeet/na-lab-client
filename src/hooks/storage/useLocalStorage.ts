b3fba28de58f3800e8b84831d7ef0f28a1eda6bf
sumi-0011 <selina2000@naver.com>
2023-05-26 13:47:15 +0900

    feat: useLocalStorage hook 생성

---
+++ src/hooks/storage/useLocalStorage.ts
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+/**
+ * @description 페이지 새로 고침을 통해 상태가 유지되도록 로컬 저장소에 동기화합니다.
+ *
+ * @param key 로컬 저장소에 저장될 키
+ * @param initialValue 초기 값
+ * @returns [storedValue, setValue] - 로컬 저장소에 저장된 값, 저장 함수
+ */
+function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+}
+
+export default useLocalStorage;
