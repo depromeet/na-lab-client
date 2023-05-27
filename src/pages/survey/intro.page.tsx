0fc61629b89a23cf037737e3805eebf629bf292f
sumi-0011 <selina2000@naver.com>
2023-05-27 14:21:06 +0900

    feat: intro -> create

--- src/pages/survey/intro.page.tsx
+++ src/pages/survey/intro.page.tsx
@@ -1,7 +1,14 @@
 import Intro from '~/features/survey/Intro';
+import useInternalRouter from '~/hooks/router/useInternalRouter';
 
 const IntroPage = () => {
-  return <Intro />;
+  const router = useInternalRouter();
+
+  const next = () => {
+    router.push('/survey/create');
+  };
+
+  return <Intro next={next} />;
 };
 
 export default IntroPage;
