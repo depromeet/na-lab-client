d3266c3e1d1665cfd275312680bf52f85a4ab6c4
Hyesung Oh <haesungoh414@gmail.com>
2023-05-31 01:22:55 +0900

    chore: invalid dom property

--- src/components/graphic/softskills/결단력_있는.tsx
+++ src/components/graphic/softskills/결단력_있는.tsx
@@ -8,8 +8,8 @@ export const 결단력_있는 = ({ color = '#C9CFDF' }: ComponentProps<typeof Sv
     <Svg size={20} isUsingFill color={color}>
       <circle cx="10" cy="10" r="5" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
       />
     </Svg>
