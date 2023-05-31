f580fddb0b4232f481adf5f3668e60b07efdccef
Hyesung Oh <haesungoh414@gmail.com>
2023-05-31 23:11:32 +0900

    style: z-index theme 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,17 @@
 import colors from '~/styles/color';
 
+const zIndex = {
+  belowDefault: -1,
+  default: 0,
+  aboveDefault: 1,
+  belowFixed: 99,
+  fixed: 100,
+  aboveFixed: 101,
+  backdrop: 900,
+  modal: 1000,
+  toast: 2000,
+} as const;
+
 const theme = {
   colors,
   size: {
@@ -8,6 +20,7 @@ const theme = {
   transition: {
     defaultEasing: 'cubic-bezier(0.6, -0.05, 0.01, 0.99)',
   },
+  zIndex,
 } as const;
 
 export default theme;
