624741d52af8eb2d19ff1b5c4cf4fce3c7f9f565
sumi.byun <selina2000@naver.com>
2023-05-31 22:05:02 +0900

    Toast 메시지 z-index 추가 (#176)
    * feat: toast 구현
    
    * chore: 스토리북 버튼 수정
    
    * feat: 토스트 박스 바텀시트 보다 위에 오도록
    
    ---------
    
    Co-authored-by: sjoleee <cent7425@gmail.com>
    Co-authored-by: hyesung oh <haesungoh414@gmail.com>

--- src/components/toast/ToastWrapper.tsx
+++ src/components/toast/ToastWrapper.tsx
@@ -38,6 +38,8 @@ export default ToastWrapper;
 
 const toastContainerCss = (higherThanCTA: boolean) => css`
   position: fixed;
+  z-index: 2000;
+
   bottom: ${higherThanCTA ? CTA_HEIGHT + CTA_PADDING : BOTTOM_SAFETY_HEIGHT}px;
   left: 0;
 
