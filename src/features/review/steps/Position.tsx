1bef855f9d8ec3f705384423817df44e22f9b810
Hyesung Oh <haesungoh414@gmail.com>
2023-05-31 20:38:59 +0900

    feat: 기본 마크업

--- src/features/review/steps/Position.tsx
+++ src/features/review/steps/Position.tsx
@@ -1,7 +1,12 @@
-import { Perspective } from '@egjs/flicking-plugins';
+import { Fade, Perspective } from '@egjs/flicking-plugins';
 import Flicking from '@egjs/react-flicking';
 import { css } from '@emotion/react';
+import { m } from 'framer-motion';
 
+import { defaultFadeInVariants } from '~/constants/motions';
+
+import BottomNavigation from '../BottomNavigation';
+import QuestionHeader from '../QuestionHeader';
 import { type StepProps } from './type';
 
 import '@egjs/react-flicking/dist/flicking.css';
@@ -10,38 +15,53 @@ interface Props extends StepProps {
   position?: string;
 }
 
-const Position = ({}: Props) => {
-  const plugins = [new Perspective({ rotate: 0.5, scale: 0.5 })];
+const Position = ({ prev, next }: Props) => {
+  const plugins = [new Perspective({ rotate: 0.2, scale: 0.2 }), new Fade('', 1.1)];
 
   return (
-    <section>
-      <div
-        css={css`
-          height: 100px;
-        `}
-      />
-
-      <Flicking align="center" circular={true} plugins={plugins}>
-        <div key="1" className="panel" css={testCss}>
-          1
-        </div>
-        <div key="2" className="panel" css={testCss}>
-          2
-        </div>
-        <div key="3" className="panel" css={testCss}>
-          3
-        </div>
-      </Flicking>
-    </section>
+    <>
+      <QuestionHeader title="당신의 포지션을 알려주세요." />
+      <m.section css={sectionCss} variants={defaultFadeInVariants} initial="initial" animate="animate" exit="exit">
+        <Flicking align="center" plugins={plugins} css={positionWrapperCss}>
+          <div key="1" className="panel" css={testCss}>
+            1
+          </div>
+          <div key="2" className="panel" css={testCss}>
+            2
+          </div>
+          <div key="3" className="panel" css={testCss}>
+            3
+          </div>
+        </Flicking>
+      </m.section>
+      <BottomNavigation onBackClick={prev} onNextClick={next} />
+    </>
   );
 };
 
 export default Position;
+
+const sectionCss = css`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: center;
+
+  height: 100%;
+`;
+
+const positionWrapperCss = css`
+  position: fixed;
+  left: 0;
+  width: 100vw;
+`;
 
 const testCss = css`
   width: 210px;
   height: 280px;
 
   /* margin-right: 30px; */
-  background-color: red;
+  background-color: blue;
 `;
