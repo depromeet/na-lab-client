7f3a0dfbce88e374c7048070bbab399359f897bf
sumi-0011 <selina2000@naver.com>
2023-06-02 21:40:14 +0900

    feat: 파랑 강조 글씨 추가

--- src/pages/survey/link.page.tsx
+++ src/pages/survey/link.page.tsx
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, type Theme } from '@emotion/react';
 import { m } from 'framer-motion';
 
 import CTAButton from '~/components/button/CTAButton';
@@ -27,10 +27,9 @@ const SurveyLinkPage = () => {
   return (
     <main css={mainCss}>
       <StaggerWrapper wrapperOverrideCss={fixedContainerCss(25)}>
-        <p>회원가입 하고</p>
-        <p>당신의 커리어 질문 폼을</p>
+        <p>아래 공유하기 버튼을 눌러</p>
         <p>
-          동료들에게 <strong>공유</strong>하세요!
+          <strong css={strongCss}>나의 질문 폼 링크</strong>를 복사하세요!
         </p>
       </StaggerWrapper>
       <section css={fixedContainerCss(58)}>
@@ -41,7 +40,9 @@ const SurveyLinkPage = () => {
 
       {/* TODO : 카카오 회원가입 버튼 스타일 변경  */}
       <m.div css={fixedBottomCss} variants={CTAVariants} initial="initial" animate="animate" exit="exit">
-        <CTAButton onClick={onNext}>공유하기</CTAButton>
+        <CTAButton color="blue" onClick={onNext}>
+          공유하기
+        </CTAButton>
       </m.div>
     </main>
   );
@@ -52,4 +53,8 @@ export default SurveyLinkPage;
 const mainCss = css`
   width: 100%;
   height: 100vh;
+`;
+
+const strongCss = (theme: Theme) => css`
+  color: ${theme.colors.primary_200};
 `;
