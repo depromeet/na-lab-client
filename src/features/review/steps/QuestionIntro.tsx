4ec670a7680aae0d2c3e133b694093435a10aa5e
hyesung oh <haesungoh414@gmail.com>
2023-06-07 21:46:35 +0900

    review - question intro 배경 이미지 및 스타일 변경 (#210)
    * chore: 이미지 업데이트
    
    * style: question intro 배경 이미지 스타일

--- src/features/review/steps/QuestionIntro.tsx
+++ src/features/review/steps/QuestionIntro.tsx
@@ -57,11 +57,17 @@ const wrapperCss = css`
 `;
 
 const pictureCss = (theme: Theme) => css`
-  position: absolute;
+  position: fixed;
   z-index: ${theme.zIndex.belowDefault};
   top: 0;
-  left: 0;
+  left: 50%;
+  transform: translateX(-50%);
 
   width: 100%;
+  max-width: ${theme.size.maxWidth};
   height: 100%;
+
+  & > img {
+    object-fit: cover;
+  }
 `;
