08b8fa8e737e6e6e831d9d798ec583c503a369d2
Hyesung Oh <haesungoh414@gmail.com>
2023-06-10 16:29:36 +0900

    fix: font cdnjs 이용

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -149,6 +149,9 @@ const globalStyles = css`
   ${resetCss};
 
   :root {
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
     color: ${theme.colors.black};
   }
 
