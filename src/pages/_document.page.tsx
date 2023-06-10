a0375c0adb25a1d38930f87a66196360c86b0e24
hyesung oh <haesungoh414@gmail.com>
2023-06-11 01:35:42 +0900

    font 적용 (#232)
    * fix: disable next font
    
    * fix: font cdnjs 이용

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="kr">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
