352012d1487b8eb2ccf77a4ee8915fa1053247c7
권오연 <61301574+oyeon-kwon@users.noreply.github.com>
2023-06-10 22:36:39 +0900

    최초 진입에서 로그인 화면 (#104)
    * feat: 로그인 화면 구현 진행 중
    
    * feat: 로그인 페이지 구현
    
    * fix: 코드 리뷰 반영
    
    * fix: 로그인 레이아웃 변경 중
    
    * feat: 로그인 후 서버로 데이터 보내주는 코드 추가
    
    * fix: 응답 type 지정
    
    * fix: 디자인 없는 컴포넌트 스토리북 삭제
    
    * fix: 토큰 함수 분리
    
    * fix: 코드 정리
    
    * feat: 카카오 로그인 시 로컬스토리지에 액세스토큰 저장
    
    * fix: token 타입 지정

--- src/components/kakaoLoginButton/KakaoLoginButton.tsx
+++ src/components/kakaoLoginButton/KakaoLoginButton.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { css } from '@emotion/react';
+
+import { post } from '~/libs/api';
+import colors from '~/styles/color';
+import { BODY_1 } from '~/styles/typo';
+
+interface Token {
+  token_type: string;
+  access_token: string;
+}
 
 const KakaoLoginButton = () => {
   const { data: session } = useSession();
 
+  const getTokenHandler = async () => {
+    const token: Token = await post('/oauth/kakao', {
+      nickname: session?.user?.name,
+      email: session?.user?.email,
+    });
+
+    localStorage.setItem('na_lab_access_token', token.access_token);
+  };
+
+  const logOutHandler = () => {
+    signOut();
+    localStorage.removeItem('na_lab_access_token');
+  };
+
+  useEffect(() => {
+    if (session) {
+      getTokenHandler();
+    }
+  }, [session]);
+
+  // TODO: 로그아웃 기능은 현재 없음, 추후 필요시 가공 필요
   if (session) {
-    // TODO : 추후 서버로 session.user.name 과 session.user.email 정보를 보내줄 수 있음
     return (
-      <>
-        <div>{session.user?.name}님 환영합니다.</div>
-        <button type="button" onClick={() => signOut()}>
+      <div css={KakaoLoginWrapper}>
+        <button type="button" onClick={logOutHandler}>
           로그아웃 하기
         </button>
-      </>
+      </div>
     );
   }
 
   return (
-    <button type="button" onClick={() => signIn('kakao')}>
-      카카오로 로그인 하기
-    </button>
+    <div css={KakaoLoginWrapper}>
+      이미 계정이 있다면?
+      <button type="button" css={KakaoLoginButtonCss} onClick={() => signIn('kakao')}>
+        카카오 로그인하기
+      </button>
+    </div>
   );
 };
+
+const KakaoLoginWrapper = css`
+  line-height: 24px;
+  color: ${colors.gray_400};
+`;
+
+const KakaoLoginButtonCss = css`
+  cursor: pointer;
+
+  overflow: visible;
+
+  margin-left: 5px;
+  padding: 0;
+  ${BODY_1}
+
+  color: ${colors.gray_400};
+  text-decoration: underline;
+
+  background: inherit;
+  border: none;
+  border-radius: 0;
+  box-shadow: none;
+`;
 
 export default KakaoLoginButton;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,21 +1,9 @@
 import { type ReactElement } from 'react';
-import Link from 'next/link';
 
 import LayoutPaddingTo23 from '~/components/layout/LayoutPaddingTo23';
 
 export default function Home() {
-  return (
-    <ul>
-      <li>
-        <Link href="/review/foo-bar">foo</Link>
-      </li>
-      <li>
-        <Link href="/survey">나만의 질문 만들기</Link>
-      </li>
-
-      {/* <ChangeQuestion /> */}
-    </ul>
-  );
+  return <div>Home</div>;
 }
 
 Home.getLayout = (page: ReactElement) => <LayoutPaddingTo23>{page}</LayoutPaddingTo23>;
