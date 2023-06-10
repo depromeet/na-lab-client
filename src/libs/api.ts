a0dbb8bcd02beaa03f960a27f3c212a7805e32c3
Hyesung Oh <haesungoh414@gmail.com>
2023-06-10 17:49:14 +0900

    style: image quilty

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -7,7 +7,8 @@ import { type ApiErrorScheme } from '~/exceptions/type';
 import { isProd } from '~/utils/common';
 
 const DEVELOPMENT_API_URL = 'https://api.nalab.me/mock';
-const PRODUCTION_API_URL = 'https://api.nalab.me/v1';
+// const PRODUCTION_API_URL = 'https://api.nalab.me/v1';
+const PRODUCTION_API_URL = 'https://api.nalab.me/mock';
 
 const instance = axios.create({
   baseURL: isProd(process.env.NODE_ENV) ? PRODUCTION_API_URL : DEVELOPMENT_API_URL,
