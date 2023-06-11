16e296916f25eda993c9ea4526bbc3bb7ef26667
Hyesung Oh <haesungoh414@gmail.com>
2023-06-11 21:34:59 +0900

    fix: lint

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -7,7 +7,6 @@ import { errorMessage } from '~/exceptions/messages';
 import { type ApiErrorScheme } from '~/exceptions/type';
 import { isProd } from '~/utils/common';
 
-
 const DEVELOPMENT_API_URL = 'https://api.nalab.me/v1';
 const PRODUCTION_API_URL = 'https://api.nalab.me/v1';
 
