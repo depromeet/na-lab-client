65bb26adfc3d9b06ac489e766ab51a68b788cf2f
Hyesung Oh <haesungoh414@gmail.com>
2023-06-12 01:51:32 +0900

    feat: use-local-storage 이전 값 보장 가능하도록 변경

--- src/hooks/storage/useLocalStorage.ts
+++ src/hooks/storage/useLocalStorage.ts
@@ -25,11 +25,17 @@ function useLocalStorage<T>(key: string, initialValue: T) {
 
   const setValue = (value: T | ((val: T) => T)) => {
     try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      }
+      setStoredValue((prev) => {
+        const nextValue = value instanceof Function ? value(prev) : value;
+
+        if (nextValue === null) {
+          localStorage.removeItem(key);
+        } else {
+          localStorage.setItem(key, JSON.stringify(nextValue));
+        }
+
+        return nextValue;
+      });
     } catch (error) {
       console.error(error);
     }
