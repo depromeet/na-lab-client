2de1b838818de65409ba0b7a4a68021324afd86a
Hyesung Oh <haesungoh414@gmail.com>
2023-06-12 05:35:32 +0900

    feat: 질문폼의 모든 피드백 조회 api

--- src/hooks/api/feedbacks/useGetFeedbackById.ts
+++ src/hooks/api/feedbacks/useGetFeedbackById.ts
@@ -2,15 +2,19 @@ import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 
 import { get } from '~/libs/api';
 
-interface Request {
+interface Response {
   feedback_id: number;
   created_at: string;
   reviewer: Reviewer;
   question: QuestionWithIsRead[];
 }
 
-const useGetFeedbackById = (id: number, options?: UseQueryOptions<Request>) => {
-  return useQuery<Request>({ queryKey: ['feedback', id], queryFn: () => get<Request>(`/feedbacks/${id}`), ...options });
+const useGetFeedbackById = (id: number, options?: UseQueryOptions<Response>) => {
+  return useQuery<Response>({
+    queryKey: ['feedback', id],
+    queryFn: () => get<Response>(`/feedbacks/${id}`),
+    ...options,
+  });
 };
 
 export default useGetFeedbackById;
--- src/remotes/reviewer.d.ts
+++ src/remotes/reviewer.d.ts
@@ -1,7 +1,14 @@
 type ReviewerPosition = 'designer' | 'developer' | 'product-manager' | 'others';
 
+/**
+ * reviewer_id 없음
+ */
 interface Reviewer {
   nickname: string;
   collaboration_experience: boolean;
   position: ReviewerPosition;
+}
+
+interface ReviewerWithId extends Reviewer {
+  reviewer_id: number;
 }
