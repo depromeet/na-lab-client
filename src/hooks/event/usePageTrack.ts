5fcb98ad5e544d1384064157c9d928f20651418b
hyesungoh <haesungoh414@gmail.com>
2023-07-01 15:38:03 +0900

    fix: 프로덕션 환경 아닐 때 페이지 트랙 비활성화

--- src/hooks/event/usePageTrack.ts
+++ src/hooks/event/usePageTrack.ts
@@ -3,11 +3,14 @@ import { useRouter } from 'next/router';
 import mixpanel from 'mixpanel-browser';
 
 import { pageview } from '~/libs/gtag';
+import { isProd } from '~/utils/common';
 
 const usePageTrack = () => {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
+      if (!isProd(process.env.NODE_ENV)) return;
+
       pageview(url);
       mixpanel.track('Page view', { label: url, category: process.env.WEB_VERSION });
     };
