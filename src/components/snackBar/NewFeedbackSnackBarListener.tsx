fa16acd1978603173b67fc8f33c6176dd517a1c0
hyesungoh <haesungoh414@gmail.com>
2023-07-01 16:36:02 +0900

    feat: new feedback snack bar listener 개발 및 적용

---
+++ src/components/snackBar/NewFeedbackSnackBarListener.tsx
@@ -0,0 +1,61 @@
+import { type FC, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { css } from '@emotion/react';
+
+import useGetFeedbackSummaryBySurveyId from '~/hooks/api/feedbacks/useGetFeedbackSummaryBySurveyId';
+import useGetSurveyIdByUserStatus from '~/hooks/api/surveys/useGetSurveyIdByUserStatus';
+import useInternalRouter from '~/hooks/router/useInternalRouter';
+
+import useSnackBar from './useSnackBar';
+
+const 새_피드백_스낵바_뜨기_전_시간 = 1500;
+
+const NewFeedbackSnackBarListener: FC = () => {
+  const { status } = useSession();
+
+  const { data, isSuccess } = useGetSurveyIdByUserStatus();
+  const { data: feedbackSummary } = useGetFeedbackSummaryBySurveyId(data?.survey_id as string, {
+    enabled: isSuccess,
+  });
+
+  const { fireSnackBar } = useSnackBar();
+
+  const router = useInternalRouter();
+
+  useEffect(() => {
+    if (status === 'loading') return;
+    if (status === 'unauthenticated') return;
+    if (!feedbackSummary) return;
+
+    if (feedbackSummary.new_feedback_count > 0) {
+      setTimeout(() => {
+        fireSnackBar({
+          content: (
+            <>
+              새로운 피드백이 도착했어요! <span css={tagCss}>+{feedbackSummary.new_feedback_count}</span>
+            </>
+          ),
+          onClick: () => {
+            router.push('/result');
+          },
+        });
+      }, 새_피드백_스낵바_뜨기_전_시간);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [feedbackSummary, fireSnackBar, status]);
+
+  return null;
+};
+
+export default NewFeedbackSnackBarListener;
+
+const tagCss = css`
+  display: inline-block;
+
+  height: 24px;
+  margin-left: 10px;
+  padding: 0 8px;
+
+  background-color: #f85b81;
+  border-radius: 24px;
+`;
