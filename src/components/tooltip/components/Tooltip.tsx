7a67ee4bc14fae3f93004b22107f3c93c14a3f81
sjoleee <cent7425@gmail.com>
2023-07-07 01:28:43 +0900

    feat: 닫혔을때 실행될 함수 로직 추가

--- src/components/tooltip/components/Tooltip.tsx
+++ src/components/tooltip/components/Tooltip.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { TooltipProvider } from '../contexts/TooltipContext';
 import { type TooltipProps } from '../types';
 import { FloatingTooltip } from './Tooltip/FloatingTooltip';
 
 export function ControlledTooltip(props: TooltipProps) {
-  const { placement, offset, onClickOutside, interactive } = props;
+  const { placement, offset, onClickOutside, interactive, isShowing, onClose } = props;
+
+  useEffect(() => {
+    if (!isShowing && onClose) {
+      onClose();
+    }
+  }, [isShowing, onClose]);
 
   return (
     <TooltipProvider
@@ -22,16 +28,13 @@ export function ControlledTooltip(props: TooltipProps) {
 export function UncontrolledTooltip({ ...props }: TooltipProps) {
   const [isShowing, setIsShowing] = useState(true);
 
-  return (
-    <ControlledTooltip
-      {...props}
-      isShowing={isShowing}
-      onClose={() => setIsShowing(false)}
-      onChildrenClick={() => setIsShowing(!isShowing)}
-    />
-  );
+  return <ControlledTooltip {...props} isShowing={isShowing} onChildrenClick={() => setIsShowing(!isShowing)} />;
 }
 
+/**
+ * @description 툴팁을 부착하려는 요소를 <Tooltip>으로 감싸주세요.
+ * isShowing을 넘겨주지 않으면 Uncontrolled 방식의 툴팁을 사용할 수 있습니다.
+ */
 export function Tooltip(props: TooltipProps) {
   if (props.isShowing !== undefined) {
     return <ControlledTooltip {...props} />;
