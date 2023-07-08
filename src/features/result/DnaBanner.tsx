b060b4f9b36f2cc17bdec526eb15e7fae7dcce18
hyesungoh <haesungoh414@gmail.com>
2023-07-08 17:08:28 +0900

    feat: 결과 페이지 dna 배너 피드백 없을 때

---
+++ src/features/result/DnaBanner.tsx
@@ -0,0 +1,108 @@
+import { type FC } from 'react';
+import Image from 'next/image';
+import { css, type Theme } from '@emotion/react';
+
+import HideIcon from '~/components/icons/HideIcon';
+import { HEAD_3_SEMIBOLD } from '~/styles/typo';
+
+interface Props {
+  responseCount: number;
+}
+
+const MINIMUM_RESPONSE_NUMBER = 3;
+
+const DnaBanner: FC<Props> = ({ responseCount }) => {
+  if (responseCount < MINIMUM_RESPONSE_NUMBER) {
+    return <NotEnoughResponse responseCount={responseCount} />;
+  }
+
+  return <section css={sectionBaseCss}>{responseCount}</section>;
+};
+
+export default DnaBanner;
+
+interface NotEnoughResponseProps {
+  responseCount: number;
+}
+
+const NotEnoughResponse: FC<NotEnoughResponseProps> = ({ responseCount }) => {
+  return (
+    <section css={[sectionBaseCss, notEnoughSectionCss]}>
+      <HideIcon />
+      <p css={paragraphCss}>
+        나의 커리어 DNA 추출을 위한
+        <br />
+        피드백 데이터가 아직 부족해요.
+      </p>
+      <small css={smallCss}>
+        최소 피드백 갯수 <strong>{responseCount}</strong>/{MINIMUM_RESPONSE_NUMBER}
+      </small>
+
+      <Image priority quality={100} css={imageCss} src="/images/dna/05_banner.webp" alt="na lab" fill />
+      <div css={imageBlurCss} />
+    </section>
+  );
+};
+
+const sectionBaseCss = css`
+  transform: translateX(-23px);
+  width: calc(100% + 23px + 23px);
+  height: 170px;
+  background-color: lightgreen;
+`;
+
+const notEnoughSectionCss = css`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const paragraphCss = (theme: Theme) => css`
+  ${HEAD_3_SEMIBOLD}
+
+  margin-top: 4px;
+  margin-bottom: 17px;
+  color: ${theme.colors.secondary_200};
+  text-align: center;
+`;
+
+const smallCss = (theme: Theme) => css`
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 100%;
+  color: ${theme.colors.secondary_200};
+  letter-spacing: -0.3px;
+
+  & > strong {
+    font-weight: 500;
+    color: ${theme.colors.primary_200};
+  }
+`;
+
+const imageCss = css`
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  object-fit: cover;
+`;
+
+const imageBlurCss = css`
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background: rgb(217 227 252 / 60%);
+  backdrop-filter: blur(15px);
+`;
