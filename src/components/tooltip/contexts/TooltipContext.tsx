14b156b5b39c336549c5ed06e94ce51acbe515f1
Sangjo LEE <cent7425@gmail.com>
2023-07-13 01:45:54 +0900

    Tooltip 컴포넌트 작성 (#362)
    * feat: useIsMounted 작성 (#71)
    
    * feat: tooltip 컴포넌트 작성
    
    * feat: onChildrenClick 이벤트
    
    * docs: type doc
    
    * feat: 닫혔을때 실행될 함수 로직 추가
    
    * feat: 애니메이션 수정

---
+++ src/components/tooltip/contexts/TooltipContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, type ReactNode, type RefObject, useContext, useRef } from 'react';
+
+import { useOnClickOutside } from '../hooks/useOnClickOutside';
+
+interface TooltipProviderProps {
+  children: ReactNode;
+  placement?: 'top' | 'bottom';
+  offsetNumber?: number;
+  interactive?: boolean;
+  onClickOutside?: () => void;
+}
+
+interface State {
+  tooltipBaseRef: RefObject<HTMLDivElement> | null;
+  targetRef: RefObject<HTMLDivElement> | null;
+  floatingRef: RefObject<HTMLDivElement> | null;
+  offset: number;
+}
+
+export const TooltipContext = createContext<State | null>(null);
+
+export function TooltipProvider({ children, offsetNumber = 0, onClickOutside, interactive }: TooltipProviderProps) {
+  const tooltipBaseRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+  const floatingRef = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside({ ref: tooltipBaseRef, onClickOutside, interactive });
+
+  return (
+    <TooltipContext.Provider
+      value={{
+        targetRef,
+        tooltipBaseRef,
+        floatingRef,
+        offset: offsetNumber,
+      }}
+    >
+      {children}
+    </TooltipContext.Provider>
+  );
+}
+
+export function useTooltip() {
+  const tooltip = useContext(TooltipContext);
+
+  return tooltip ?? DEFAULT_VALUE;
+}
+
+const DEFAULT_VALUE = {
+  tooltipBaseRef: null,
+  targetRef: null,
+  floatingRef: null,
+  offset: null,
+};
