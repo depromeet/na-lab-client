7bb1b7ff70a73e8f9bf27d6ce829c3b4f93f1e95
sumi-0011 <selina2000@naver.com>
2024-03-10 22:46:10 +0900

    refactor: 주석 제거

--- src/hooks/api/gallery/useGetMyBookmarkList.tsx
+++ src/hooks/api/gallery/useGetMyBookmarkList.tsx
@@ -1,26 +1,12 @@
-// const useGetGalleryList = (request: Request, options?: UseQueryOptions<Response>) => {
-//   return useQuery<Response>({
-//     queryKey: ['gallerys', request],
-//     queryFn: () =>
-//       get<Response>(
-//         `/v1/gallerys?page=${request.page}&job=${request.position}&order-type=${request.order_type}&count=${request.count}`,
-//       ),
-//     ...options,
-//   });
-// };
-
 import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 
 import { get } from '~/libs/api';
 
-// /v1/surveys/bookmarks?last-survey-id={survey_id}&order-type={order_type}&count={count}
 type OrderType = 'latest' | 'update';
 
 interface Request {
   order_type: OrderType;
   // TODO : 페이징 추가
-  // last_survey_id: string;
-  // count?: number;
 }
 
 export interface BookmarkedSurveyType {
